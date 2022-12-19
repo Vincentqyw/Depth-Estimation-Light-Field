0c9664dcc3ca7b2a539c4ee97365bef09d8b634c
Realcat <realcat@126.com>
2022-12-19 04:03:38 +0000

    add: submodules

---
+++ assets/run.sh
@@ -0,0 +1,9 @@
+git submodule add https://github.com/YingqianWang/OACC-Net.git thirdparty/OACC-Net
+# git submodule add https://github.com/brownvc/lightfielddepth.git thirdparty/lightfielddepth
+git submodule add https://github.com/mediaeng-lfv/LFV_Disparity_Estimation.git thirdparty/LFV
+git submodule add https://github.com/shuozh/Spinning-Parallelogram-Operator.git thirdparty/SPO
+git submodule add https://github.com/YanWQ/MANet.git thirdparty/MANet
+git submodule add https://github.com/lkyahpu/EPI_ORM.git thirdparty/EPI_ORM
+git submodule add https://github.com/YanWQ/LLF-Net.git.git thirdparty/LLF-Net
+git submodule add https://github.com/titus-leistner/epi-shift.git thirdparty/EPI-SHIFT
+git submodule add https://github.com/chshin10/epinet.git thirdparty/EPINET
